947cd7f569790c45704c79f1d809e701de397a27
jacek <mateuszjakoniuk@outlook.com>
2024-01-15 16:02:45 +0100

    little more of bmp

--- src/steg_core/formats/bmp/parser.rs
+++ src/steg_core/formats/bmp/parser.rs
@@ -1,12 +1,9 @@
-#[derive(Debug)]
-pub struct Header{
-    format : u16,
-    size : u32,
-    special1 : u16,
-    special2 : u16,
-    offset : u32,
-}
-#[derive(Debug)]
-pub struct Bmp {
-    header: Header,
+use crate::steg_core::formats::bmp::definitions;
+use definitions::Bmp;
+use anyhow::Result;
+
+impl Bmp {
+    pub fn parse() -> Result<Bmp>{
+        todo!()
+    }
 }
